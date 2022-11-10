a12c8f41bf4ba7706c4ad56b85094c9c365d34c5
jaekeun Choi <jaek1783@gmail.com>
2022-11-10 21:16:56 +0900

    필터 form 추가

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,10 +1,13 @@
 import modules from './button.module.css';
 import Link from 'next/link';
 const Button = (props)=>{
-    return(
-        <Link href={props.link}>
-            <a className={modules.btn}>{props.children}</a>
-        </Link>
-    )
+    if(props.link){
+        return(
+            <Link href={props.link}>
+                <a className={modules.btn}>{props.children}</a>
+            </Link>
+        )
+    }
+    return <button className={modules.btn} onClick={props.onClick}>{props.children}</button>
 };
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,11 @@
 import {getAllEvents} from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventSearch from './event-search';
 const AllEventsPage = ()=>{
     const allEvents = getAllEvents();
     return(
         <div>
+            <EventSearch/>
             <EventList items={allEvents}/>
         </div>
     )
