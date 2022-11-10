1b79f2ff15c7c5dd7e7b52ee38f6aa8327016269
jaekeun Choi <jaek1783@gmail.com>
2022-11-10 19:36:39 +0900

    detail page 추가

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,31 @@
+import { Fragment } from 'react';
+import {useRouter} from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 const EventDetailPage = () => {
+    const router = useRouter();
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+    if(!event){
+        return(
+            <p>No Found Page</p>
+        );
+    }
     return(
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title}/>
+            <EventLogistics
+                image = {event.image}
+                imageAlt={event.title}
+                date = {event.date}
+                address={event.location}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     )
 };
 export default EventDetailPage;
