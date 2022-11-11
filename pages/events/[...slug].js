9cda8bdb2b76c51ce20f2d518bf831b42064e7f3
jaekeun Choi <jaek1783@gmail.com>
2022-11-11 12:44:02 +0900

    프로젝트 완성

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,26 @@
+import { Fragment } from 'react';
 import {useRouter} from 'next/router';
+
 import EventList from '../../components/events/event-list';
 import {getFilteredEvents} from '../../dummy-data';
+import ResultTitle from '../../components/events/results-title';
+import EerrorAlert from '../../components/ui/error-alert';
+import Button from '../../components/ui/button';
+
 const FilterEventPage = () => {
     const router = useRouter();
     const filterData = router.query.slug;
 
     if(!filterData){
-        return <p className='center'>...Loading</p>
+        return <Fragment>
+                    <EerrorAlert>
+                        <p className='center'>...Loading</p>
+                    </EerrorAlert>
+                    <div className="center">
+                        <Button link='/events'>Show All Events</Button>
+                    </div>
+               </Fragment> 
+        
     }
     const filteredYear = filterData[0];
     const filteredMonth = filterData[1];
@@ -22,7 +36,15 @@ const FilterEventPage = () => {
         numMonth > 12 ||
         numMonth < 1 
     ){
-         return <p>Please checking Your Date</p>
+         return <Fragment>
+                    <EerrorAlert>
+                        <p>Please checking Your Date</p>
+                    </EerrorAlert>
+                    <div className="center">
+                        <Button link='/events'>Show All Events</Button>
+                    </div>
+                </Fragment>
+         
     }
 
    const filteredEvents = getFilteredEvents({
@@ -31,13 +53,23 @@ const FilterEventPage = () => {
    });
 // console.log(filteredEvents);
    if(!filteredEvents || filteredEvents.length === 0){
-    return <p>Sorry, notFound chosen filter!</p>
+    return <Fragment>
+        <EerrorAlert>
+            <p>Sorry, notFound chosen filter!</p>
+        </EerrorAlert>
+        <div className="center">
+            <Button link='/events'>Show All Events</Button>
+        </div>
+    </Fragment>
    }
 
+   const date = new Date(numYear, numMonth -1); 
+
     return(
-        <div>
+        <Fragment>
+            <ResultTitle date={date }/>
             <EventList items={filteredEvents}/>
-        </div>
+        </Fragment>
     )
 };
 export default FilterEventPage;
